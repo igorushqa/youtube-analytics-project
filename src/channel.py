5adb4777a3c0f8f7a1a55d934f3eae9eb1d103d6
igorushqa <since1925@mail.ru>
2023-08-03 23:10:35 +0700

    HW2_youtube

--- src/channel.py
+++ src/channel.py
@@ -11,9 +11,29 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         self.channel_id = channel_id
-        self.channel = self.youtube.channels().list(
+        self.channel = Channel.get_service().channels().list(
             id=channel_id, part='snippet,statistics'
         ).execute()
+        self.id = self.channel['items'][0]['id']
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.custom_url = self.channel['items'][0]['snippet']['customUrl']
+        self.url = f'https://www.youtube.com/{self.custom_url}'
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=cls.api_key)
+
+    def to_json(self, path):
+        data = self.__dict__
+        del data['channel']
+        del data['custom_url']
+        with open(path, 'w') as f:
+            json.dump(data, f, indent=2, ensure_ascii=False)
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
