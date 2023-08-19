e55d3e28a0b09f3f18997dfdc626403ea8c4e93c
igorushqa <since1925@mail.ru>
2023-08-20 01:42:29 +0700

    HW6

--- src/video.py
+++ src/video.py
@@ -9,15 +9,22 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id: str) -> None:
-        self.video_id = video_id
-        self.playlists = Video.get_service().videos().list(
-            part='snippet,statistics',
-            id=self.video_id
-        ).execute()
-        self.title = self.playlists['items'][0]['snippet']['title']
-        self.url = f'https://www.youtube.com/{self.video_id}'
-        self.view_count = self.playlists['items'][0]['statistics']['viewCount']
-        self.like_count = self.playlists['items'][0]['statistics']['likeCount']
+        try:
+            self.video_id = video_id
+            self.playlists = Video.get_service().videos().list(
+                part='snippet,statistics',
+                id=self.video_id
+            ).execute()
+            self.title = self.playlists['items'][0]['snippet']['title']
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+        else:
+            self.url = f'https://www.youtube.com/{self.video_id}'
+            self.view_count = self.playlists['items'][0]['statistics']['viewCount']
+            self.like_count = self.playlists['items'][0]['statistics']['likeCount']
 
     def __str__(self):
         return self.title
